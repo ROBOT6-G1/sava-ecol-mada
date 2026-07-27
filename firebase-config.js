b3379260b24bf9e0361381a980c25c9ed962234d
ROBOT6-G1 <b.outiquemevasoa@gmail.com>
2026-07-27 09:25:05 +0300

    Update firebase-config.js via DEVWEBIA

---
+++ firebase-config.js
@@ -0,0 +1,3 @@
+// Configuration LocalStorage ho an'ny ÉCOLE PRIVÉE SAVA / Académie Excelsior (Free Plan)
+// Ny mpanjifa dia afaka mampifandray ny Firebase-ny any aoriana amin'ny paramètre raha ilaina.
+console.log("ÉCOLE PRIVÉE SAVA: LocalStorage active ho an'ny fitehirizana angon-drakitra.");
